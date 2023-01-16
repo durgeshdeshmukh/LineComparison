43444b02918abb6239cd581705bab9b944582bb4
durgeshdeshmukh <durgesh.deshmukh2@gmail.com>
2023-01-15 23:02:11 -0800

    Equality of lines

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //UC1_LengthOfLine.LengthOfLine();
-            //UC2_EqualityOfLines.EqualityOfLines();
-            UC3_ComparisonOfLines.ComparisonOfLines();
+            UC2_EqualityOfLines.EqualityOfLines();
         }
     }
 }
